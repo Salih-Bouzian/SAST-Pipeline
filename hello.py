473ece0fd3e197b4fc95f2bbd9b88a7ba16ea659
Salih Bouzian <salih.bouzian@student.ap.be>
2024-12-20 21:26:34 +0100

    hello world code

--- hello.py
+++ hello.py
@@ -1,18 +1,1 @@
-# test_script.py
-
-# Hardcoded sensitive information (to trigger secrets detection)
-API_KEY = "12345-abcdef-67890"
-
-# Vulnerable dependency (to test dependency checks)
-import yaml  # Avoid loading untrusted YAML (simulated issue)
-
-def insecure_function(password="12345"):  # Hardcoded password
-    print(f"The password is {password}")
-
-# Simulated security vulnerability (to test SAST)
-def eval_user_input(user_input):
-    eval(user_input)  # Using eval is a security risk!
-
-if __name__ == "__main__":
-    user_input = input("Enter some code to execute: ")
-    eval_user_input(user_input)
+print("hello world")
